b823be212c03915d249f2b1546caf1b4be1c7fdd
H Vidyasagar <86299037+H-Vidyasagar@users.noreply.github.com>
2023-01-05 11:47:39 +0530

    Update dijktras_network.cpp

--- dijktras_network.cpp
+++ dijktras_network.cpp
@@ -1,79 +1,86 @@
+/* From a given vertex in a weighted connected graph, find shortest paths to other vertices using Dijkstra’s algorithm. */
 
+#include<bits/stdc++.h>
+using namespace std;
+int a[30][30],n;
+int minimum(int visited[],int dist[])
+{
+	int mindis=10000, mini;
+	for(int i=0;i<n;i++)
+	{
+		if(!visited[i] && dist[i]<mindis)
+		{
+			mindis=dist[i];
+			mini=i;
+		}
+	}
+	return mini;
+}
 
- #include<bits/stdc++.h> 
-#include <limits.h> 
-#include <stdio.h> 
- using namespace std;
+void dijkstra(int src)
+{
+	int dist[n],visited[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		dist[i]=10000;
+		visited[i]=0;
+	}
+	dist[src]=0;
+	for(int i=0;i<n-1;i++)
+	{
+		int u=minimum(visited,dist);
+		visited[u]=1;
+		for(int v=0;v<n;v++)
+		{
+			if(!visited[v] && a[u][v]!=10000 && dist[u]!=10000 && (dist[u]+a[u][v])<dist[v])
+				dist[v]=dist[u]+a[u][v];
+		}
+	}
+	cout<<"Shortest paths to all other vertices from  "<<src<<" is "<<endl;
+	cout<<"Vertices\tDistance from source"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		if(i!=src)
+			cout<<i<<"\t\t"<<dist[i]<<endl;
+	}
+}
 
-#define V 9 
-  
- 
-int minDistance(int dist[], bool sptSet[]) 
-{ 
-     
-    int min = INT_MAX, min_index; 
-  
-    for (int v = 0; v < V; v++) 
-        if (sptSet[v] == false && dist[v] <= min) 
-            min = dist[v], min_index = v; 
-  
-    return min_index; 
-} 
-  
+int main()
+{
+	cout<<"Enter the no. of vertices"<<endl;
+	cin>>n;
+	cout<<"Enter the weighted adjacency matrix (enter 10000 if there is no edge)"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+			cin>>a[i][j];
+	}
+	int src;
+	cout<<"Enter the source vertex"<<endl;
+	cin>>src;
+	dijkstra(src);
+	return 0;
+}
 
-void printSolution(int dist[]) 
-{ 
-    printf("Vertex \t\t Distance from Source\n"); 
-    for (int i = 0; i < V; i++) 
-        printf("%d \t\t %d\n", i, dist[i]); 
-} 
-  
- 
-void dijkstra(int graph[V][V], int src) 
-{ 
-    int dist[V]; 
-    bool sptSet[V]; 
-    for (int i = 0; i < V; i++) 
-        dist[i] = INT_MAX, sptSet[i] = false; 
-  
-    
-    dist[src] = 0; 
-  
-     
-    for (int count = 0; count < V - 1; count++) { 
-        
-        int u = minDistance(dist, sptSet); 
-  
-        
-        sptSet[u] = true; 
-  
-        
-        for (int v = 0; v < V; v++) 
-  
-            
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-                && dist[u] + graph[u][v] < dist[v]) 
-                dist[v] = dist[u] + graph[u][v]; 
-    } 
-  
-    
-    printSolution(dist); 
-} 
-  
-
-int main() 
-{ 
-    
-    int graph[V][V] ;
-    cout<<"Enter the graph "<<endl;
-    for(int i = 0; i<V; i++)
-    {
-        for(int j = 0; j<V; j++)
-            cin>>graph[i][j];
-    }
-  
-    dijkstra(graph, 0); 
-  
-    return 0; 
-} 
-
+/* output
+Enter the no. of vertices
+5
+Enter the weighted adjacency matrix (enter 10000 if there is no edge)
+0 3 10000 7 10000
+3 0 4 2 10000
+10000 4 0 5 6
+7 2 5 0 4
+10000 10000 6 4 0
+Enter the source vertex
+0
+Shortest paths to all other vertices from  0 is 
+Vertices	Distance from source
+1		3
+2		7
+3		5
+4		9
+*/
+	
+	
+	 
